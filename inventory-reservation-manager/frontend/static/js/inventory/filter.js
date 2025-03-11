9f4c6966401e60d285d20f642e8b839aa0aaf4e0
Makhuta <matykubla@gmail.com>
2025-03-11 19:36:29 +0100

    Change/Fix
    Change:
    - make possible to filter by stock/returned
    - item image is now appropriate size
    - item name and description is now wrapping correctly when they exceeds their max size
    Fix:
    - the description for item is not not required

--- inventory-reservation-manager/frontend/static/js/inventory/filter.js
+++ inventory-reservation-manager/frontend/static/js/inventory/filter.js
@@ -1,5 +1,15 @@
+function decide_stock_display(on_warehouse_filter, itemStockedValue) {
+    if(!on_warehouse_filter) return true;
+    if(on_warehouse_filter.value == "warehouse_all") return true;
+    if(on_warehouse_filter.value == "warehouse_borrowed" && !itemStockedValue) return true;
+    if(on_warehouse_filter.value == "warehouse_returned" && itemStockedValue) return true;
+
+    return false;
+}
+
 function update_filter(f) {
     let filter = f ? f.value.toLowerCase() : "";
+    let on_warehouse_filter = document.getElementById("on_warehouse_filter");
     let rows = document.querySelectorAll("#inventory > tbody > tr");
 
     rows.forEach(row => {
@@ -7,8 +17,10 @@ function update_filter(f) {
         let titleText = titleElement ? titleElement.getAttribute("data-value").toLowerCase() : "";
         let inventoryNumberElement = row.querySelector("[data-id='item_inventory_number']");
         let inventoryNumberText = inventoryNumberElement ? inventoryNumberElement.getAttribute("data-value").toLowerCase() : "";
+        let itemStockedElement = row.querySelector("[data-id='item_stocked']");
+        let itemStockedValue = (itemStockedElement ? itemStockedElement.getAttribute("data-value").toLowerCase() : "false") == "true";
 
-        row.style.display = (titleText.includes(filter) || inventoryNumberText.includes(filter)) ? "" : "none";
+        row.style.display = ((titleText.includes(filter) || inventoryNumberText.includes(filter)) && decide_stock_display(on_warehouse_filter, itemStockedValue)) ? "" : "none";
     });
 }
 
@@ -16,8 +28,58 @@ document.getElementById("tableFilter").addEventListener("keyup", function () {
     update_filter(this);
 });
 
+function replace_class(obj, from, to) {
+    if(!obj || !obj.classList) return;
+    obj.classList.remove(from);
+    obj.classList.add(to);
+}
+
+function clean_return_filter(obj) {
+    for(var ch of obj.querySelectorAll("i")) {
+        ch.classList.add("d-none");
+    }
+    obj.querySelector("#warehouse_all").classList.remove("d-none")
+    obj.value = "warehouse_all"
+    obj.querySelector("#warehouse_all").classList.remove("d-none")
+    for(var type of ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark", "white"]) {
+        obj.classList.remove(`btn-outline-${type}`);
+    }
+    obj.classList.add(`btn-outline-warning`);
+}
+
+document.querySelector("#on_warehouse_filter").addEventListener("click", function(event) {
+    let filterInput = document.getElementById("tableFilter");
+    let on_warehouse_filter = document.getElementById("on_warehouse_filter");
+    for(var ch of on_warehouse_filter.querySelectorAll("i")) {
+        ch.classList.add("d-none");
+    }
+    switch(on_warehouse_filter.value) {
+        case "warehouse_all":
+            on_warehouse_filter.value = "warehouse_returned"
+            on_warehouse_filter.querySelector("#warehouse_returned").classList.remove("d-none")
+            replace_class(on_warehouse_filter, "btn-outline-warning", "btn-outline-success")
+            break;
+        case "warehouse_returned":
+            on_warehouse_filter.value = "warehouse_borrowed"
+            on_warehouse_filter.querySelector("#warehouse_borrowed").classList.remove("d-none")
+            replace_class(on_warehouse_filter, "btn-outline-success", "btn-outline-danger")
+            break;
+        case "warehouse_borrowed":
+            on_warehouse_filter.value = "warehouse_all"
+            on_warehouse_filter.querySelector("#warehouse_all").classList.remove("d-none")
+            replace_class(on_warehouse_filter, "btn-outline-danger", "btn-outline-warning")
+            break;
+        default:
+            clean_return_filter(on_warehouse_filter);
+            break;
+    }
+    update_filter(filterInput);
+});
+
 document.querySelector("#clean_filter").addEventListener("click", function(event) {
     let filterInput = document.getElementById("tableFilter");
     filterInput.value = "";
+    let on_warehouse_filter = document.getElementById("on_warehouse_filter");
+    clean_return_filter(on_warehouse_filter);
     update_filter(filterInput);
-})
+});
--- inventory-reservation-manager/frontend/static/js/reservations/filter.js
+++ inventory-reservation-manager/frontend/static/js/reservations/filter.js
@@ -1,5 +1,15 @@
+function decide_stock_display(on_returned_filter, itemReturnedValue) {
+    if(!on_returned_filter) return true;
+    if(on_returned_filter.value == "returned_all") return true;
+    if(on_returned_filter.value == "returned_borrowed" && !itemReturnedValue) return true;
+    if(on_returned_filter.value == "returned_returned" && itemReturnedValue) return true;
+
+    return false;
+}
+
 function update_filter(f) {
     let filter = f ? f.value.toLowerCase() : "";
+    let on_warehouse_filter = document.getElementById("on_warehouse_filter");
     let rows = document.querySelectorAll("#inventory > tbody > tr");
 
     rows.forEach(row => {
@@ -7,8 +17,10 @@ function update_filter(f) {
         let titleText = titleElement ? titleElement.getAttribute("data-value").toLowerCase() : "";
         let clientElement = row.querySelector("[data-id='item_client']");
         let clientText = clientElement ? clientElement.getAttribute("data-value").toLowerCase() : "";
+        let itemReturnedElement = row.querySelector("[data-id='item_returned']");
+        let itemReturnedValue = (itemReturnedElement ? itemReturnedElement.getAttribute("data-value").toLowerCase() : "false") == "true";
 
-        row.style.display = (titleText.includes(filter) || clientText.includes(filter)) ? "" : "none";
+        row.style.display = ((titleText.includes(filter) || clientText.includes(filter)) && decide_stock_display(on_returned_filter, itemReturnedValue)) ? "" : "none";
     });
 }
 
@@ -16,8 +28,58 @@ document.getElementById("tableFilter").addEventListener("keyup", function () {
     update_filter(this);
 });
 
+function replace_class(obj, from, to) {
+    if(!obj || !obj.classList) return;
+    obj.classList.remove(from);
+    obj.classList.add(to);
+}
+
+function clean_return_filter(obj) {
+    for(var ch of obj.querySelectorAll("i")) {
+        ch.classList.add("d-none");
+    }
+    obj.querySelector("#returned_all").classList.remove("d-none")
+    obj.value = "returned_all"
+    obj.querySelector("#returned_all").classList.remove("d-none")
+    for(var type of ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark", "white"]) {
+        obj.classList.remove(`btn-outline-${type}`);
+    }
+    obj.classList.add(`btn-outline-warning`);
+}
+
+document.querySelector("#on_returned_filter").addEventListener("click", function(event) {
+    let filterInput = document.getElementById("tableFilter");
+    let on_returned_filter = document.getElementById("on_returned_filter");
+    for(var ch of on_returned_filter.querySelectorAll("i")) {
+        ch.classList.add("d-none");
+    }
+    switch(on_returned_filter.value) {
+        case "returned_all":
+            on_returned_filter.value = "returned_returned"
+            on_returned_filter.querySelector("#returned_returned").classList.remove("d-none")
+            replace_class(on_returned_filter, "btn-outline-warning", "btn-outline-success")
+            break;
+        case "returned_returned":
+            on_returned_filter.value = "returned_borrowed"
+            on_returned_filter.querySelector("#returned_borrowed").classList.remove("d-none")
+            replace_class(on_returned_filter, "btn-outline-success", "btn-outline-danger")
+            break;
+        case "returned_borrowed":
+            on_returned_filter.value = "returned_all"
+            on_returned_filter.querySelector("#returned_all").classList.remove("d-none")
+            replace_class(on_returned_filter, "btn-outline-danger", "btn-outline-warning")
+            break;
+        default:
+            clean_return_filter(on_returned_filter);
+            break;
+    }
+    update_filter(filterInput);
+});
+
 document.querySelector("#clean_filter").addEventListener("click", function(event) {
     let filterInput = document.getElementById("tableFilter");
     filterInput.value = "";
+    let on_returned_filter = document.getElementById("on_returned_filter");
+    clean_return_filter(on_returned_filter);
     update_filter(filterInput);
 })
